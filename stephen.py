77e831c071009e333053284d5cbf4fc1c97f4aca
Stephen <stephen@stafngrimr.com>
2022-04-25 16:14:00 +0100

    updates to main page mainly, some css

--- stephen.py
+++ stephen.py
@@ -8,7 +8,12 @@ app = Flask(__name__)
 # Data DB engine queries
 engine = create_engine("sqlite+pysqlite:///data.db")
 
-@app.route('/')
+@app.route("/")
 def index():
 
     return render_template("index.html")
+
+@app.route("/update", methods=["GET", "POST"])
+def update():
+
+	return render_template("update.html")
